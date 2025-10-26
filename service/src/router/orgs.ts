76bb5a2957a912c9088b51328f01bf0f7b8c9ff7
yashgoyal0110 <yg364550@gmail.com>
2025-10-26 18:48:48 +0530

    service-updated

---
+++ service/src/router/orgs.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import { getAllOrgs } from "../controllers/GetAllOrgs";
+import { getOrgById } from "../controllers/GetSpecificOrg";
+import { addOrgs } from "../controllers/AddOrgs";
+
+const prisma = new PrismaClient();
+export const router = Router();
+
+// Attach route handler
+router.get("/orgs", getAllOrgs(prisma));
+router.get("/:orgId/details", getOrgById(prisma));
+router.post("/orgs", addOrgs(prisma));
